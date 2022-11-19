f2f0055aa961af1c4ae458da5836456f783ab1fd
Darmawan <darmawanjr88@gmail.com>
2022-11-19 13:08:55 +0800

    added other key

--- dbconfig.js
+++ dbconfig.js
@@ -4,12 +4,18 @@ const firebase = require('firebase-admin');
 
 firebase.initializeApp({
     credential: firebase.credential.cert({
+        type: process.env.TYPE,
         project_id: process.env.PROJECTID,
         private_key_id: process.env.PRIVATEKEYID,
         private_key: process.env.PRIVATEKEY
-            ? process.env.PRIVATEKEY.replace(/\\n/g, '\n')
+            ? process.env.PRIVATEKEY.replace(/\\n/g, "\n")
             : undefined,
         client_email: process.env.CLIENTEMAIL,
+        client_id: process.env.CLIENTID,
+        auth_uri: process.env.AUTHURI,
+        token_uri: process.env.TOKENURI,
+        auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+        client_x509_cert_url: process.env.client_x509_cert_url
     })
 })
 
