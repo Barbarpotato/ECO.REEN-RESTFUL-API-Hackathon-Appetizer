b340680163121cd08a7f3b29c62edf5bf1c96cdb
Darmawan <darmawanjr88@gmail.com>
2022-11-20 13:56:05 +0800

    change CORS option

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const analytics = require('./routes/analytics');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+    next();
+});
+
 app.use('/transportation', transportation);
 app.use('/analytics', analytics);
 
