4735aa17df7cec894ef35cfed9a8a59830815274
Darmawan <darmawanjr88@gmail.com>
2022-11-21 16:28:05 +0800

    fixing bugs

--- routes/analytics.js
+++ routes/analytics.js
@@ -27,35 +27,47 @@ router.route('')
             }
         });
         snapshotTrans.forEach(doc => {
-            totalEmissions += doc.data().diesel;
-            totalEmissions += doc.data().ethanol;
-            totalEmissions += doc.data().natural;
-            totalEmissions += doc.data().premium;
-            totalEmissions += doc.data().reguler;
             if (!dateTime.includes(doc.data().Date)) {
-                fuelType.push(doc.data().type);
+                dateTime.push(doc.data().Date);
             }
-            totalObject += parseInt(doc.data().total);
-        })
+        });
         for (let i = 0; i < dateTime.length; i++) {
-            const snapDate = await getEmissions.where('Date', '==', dateTime[i]).get();
-            snapDate.forEach(doc => {
-                ch4 += doc.data().CH4;
-                co2 += doc.data().CO2;
-                n20 += doc.data().N20;
-                co2eq += doc.data().totalco2eq;
-                totalObject += doc.data().total;
-            })
-            totalEmissions += ch4 + co2 + n20;
-            if (fuelType && dateTime) {
-                activityObject = ['Transportation', 'Machine'];
+            const snapDate1 = await getTrans.where('Date', '==', dateTime[i]).get();
+            if (snapDate1.size != 0) {
+                snapDate1.forEach(doc => {
+                    const fueltype = doc.data().type;
+                    co2 = doc.data()[fueltype];
+                    if (!fuelType.includes(doc.data().type)) {
+                        fuelType.push(doc.data().type);
+                    }
+                    totalObject += parseInt(doc.data().total);
+                })
+                if (!activityObject.includes("Transportation")) {
+                    activityObject.push('Transportation');
+                }
             }
+            const snapDate2 = await getEmissions.where('Date', '==', dateTime[i]).get();
+            if (snapDate2.size != 0) {
+                snapDate2.forEach(doc => {
+                    ch4 += doc.data().CH4;
+                    co2 += doc.data().CO2;
+                    n20 += doc.data().N20;
+                    co2eq += doc.data().totalco2eq;
+                    totalObject += doc.data().total;
+                })
+                if (!activityObject.includes("Machine")) {
+                    activityObject.push('Machine');
+                }
+            }
+            totalEmissions += ch4 + co2 + n20;
             data.push({
                 Date: dateTime[i], totalco2: co2, totalch4: ch4, totaln20: n20, totalco2eq: co2eq,
                 totalEmissions: totalEmissions, totalObject: totalObject,
                 finance: totalEmissions * 30, activityObject: activityObject, fuelType: fuelType,
-                recover: parseInt(totalEmissions / 0.027)
+                recover: parseInt(totalEmissions / 27)
             });
+            activityObject = [];
+            fuelType = [];
             totalEmissions = 0;
             totalObject = 0;
             ch4 = 0;
